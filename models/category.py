706177a3797ea31c5bbfdc257ef775f91a347431
IlyaMuralha <muralhakuzmenko@gmail.com>
2022-02-07 19:05:24 +0500

    Added models

---
+++ models/category.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, String, Integer, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+
+class Category(Base):
+    '''
+    Класс-модель описывает сущность категорий
+    '''
+    #
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, index=True)
+    is_active = Column(Boolean)
+
+    def __str__(self):
+        '''
+        метод возвращает строковое представление объекта класса
+        :return:
+        '''
+        return self.name
