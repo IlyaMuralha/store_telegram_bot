03d483ee25c88b5760bec42e44b7c4f0b1c8eb81
IlyaMuralha <muralhakuzmenko@gmail.com>
2022-02-08 13:04:17 +0500

    Created base handler

---
+++ handlers/handler.py
@@ -0,0 +1,16 @@
+import abc
+
+
+class Handler(metaclass=abc.ABCMeta):
+
+    def __init__(self, bot):
+        # получаем объект бота
+        self.bot = bot
+        # инициализируем разметку кнопок
+        self.keyboards = Keyboards()
+        # инициализируем менеджер для работы с БД
+        self.BD = DBManager()
+
+    @abc.abstractmethod
+    def handle(self):
+        pass
